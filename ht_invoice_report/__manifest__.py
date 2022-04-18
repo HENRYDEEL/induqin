23af8e0affa84ec62954c99d4d856b47afd548cc
janeindiran <janeindiran@gmail.com>
2022-04-18 15:54:39 +0530

    invoice

---
+++ ht_invoice_report/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Invoice Report",
+    'category': 'Tools',
+    'author': 'Hadoopt Technologies Private Limited',
+    'website': 'www.hadoopt.com',
+    'license': 'AGPL-3',
+    'summary': '',
+    "depends": [
+        "base",
+        "account"
+    ],
+    
+    'data': [
+        'report/invoice_report.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
